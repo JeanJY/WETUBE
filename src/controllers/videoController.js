e5a29570bfd63e979b881c041886768de79de9c5
Jean <jeanjyjh@gmail.com>
2024-06-30 23:03:22 +1200

    Finish Video CRUD

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,7 @@
 import Video from "../models/Video";
 
 export const home = async (req, res) => {
-  const videos = await Video.find({});
+  const videos = await Video.find({}).sort({ createdAt: "desc" });
   return res.render("home", { pageTitle: "Home", videos });
 };
 
@@ -58,6 +58,25 @@ export const postUpload = async (req, res) => {
       errorMessage: error._message,
     });
   }
+};
+
+export const deleteVideo = async (req, res) => {
+  const { id } = req.params;
+  await Video.findOneAndDelete(id);
+  return res.redirect("/");
+};
+
+export const search = async (req, res) => {
+  const { keyword } = req.query;
+  let videos = [];
+  if (keyword) {
+    videos = await Video.find({
+      title: {
+        $regex: new RegExp(keyword, "i"),
+      },
+    });
+  }
+  return res.render("search", { pageTitle: "Search", videos });
 };
 // -----------------------------
 // export const search = (req, res) => res.send("Search");
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,13 +1,13 @@
 import express from "express";
 import { join, login } from "../controllers/userController";
-import { home } from "../controllers/videoController";
+import { home, search } from "../controllers/videoController";
 
 const globalRouter = express.Router();
 
 globalRouter.get("/", home);
 globalRouter.get("/join", join);
 globalRouter.get("/login", login);
-// globalRouter.get("/search", search);
+globalRouter.get("/search", search);
 
 export default globalRouter;
 // 변수만 export 하는 중. globalRouter.js 전체가 아니라.?? default와 개별 export 차이? 라우터와 컨트롤러 따로 두기??
