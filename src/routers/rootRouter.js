8c29a78533de15fd59c515e03c830af044309189
Jean <jeanjyjh@gmail.com>
2024-07-05 16:54:04 +1200

    create user schema and hash password

--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,14 +1,14 @@
 import express from "express";
-import { join, login } from "../controllers/userController";
+import { getJoin, postJoin, login } from "../controllers/userController";
 import { home, search } from "../controllers/videoController";
 
-const globalRouter = express.Router();
+const rootRouter = express.Router();
 
-globalRouter.get("/", home);
-globalRouter.get("/join", join);
-globalRouter.get("/login", login);
-globalRouter.get("/search", search);
+rootRouter.get("/", home);
+rootRouter.route("/join").get(getJoin).post(postJoin);
+rootRouter.get("/login", login);
+rootRouter.get("/search", search);
 
-export default globalRouter;
+export default rootRouter;
 // 변수만 export 하는 중. globalRouter.js 전체가 아니라.?? default와 개별 export 차이? 라우터와 컨트롤러 따로 두기??
 // 디폴트 추출은 임폴트페이지에서 이름을 뭘로 설정하던 상관 없음
