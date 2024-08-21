d08c4794ccdfedf8f9c3ec809fac4d22c23302cb
Jean <jeanjyjh@gmail.com>
2024-08-22 09:21:35 +1200

    Implement video view counter API (1)
    Create registerView function and API route, which is a add API endpoint for video view count.

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
+import apiRouter from "./routers/apiRouter";
 import { localsMiddleware } from "./middlewares";
 
 const app = express();
@@ -35,18 +36,11 @@ app.use("/static", express.static("assets"));
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
+app.use("/api", apiRouter);
 
 export default app;
 
 //static; 브라우저에서 사람들이 볼 수 있게 함.
-
-// app.get("/", (req, res) => {
-//   return res.send("<h1>I love you.</h1>");
-// });
-
-// app.get("/login", (req, res) => {
-//   return res.end();
-// });
 
 // const handleListening = () => console.log("server listenting on port 4000");
 // app.listen(4000, handleListening);
@@ -54,7 +48,7 @@ export default app;
 // function handleListening () {
 //     return console.log(`server listenting on port http://localhost:${PORT}`);
 
-// }
+//
 // **** {}브라켓이 있고 return이 없으면 ; 함수로 산출값은 나오지만 반환하여 다음에 다른곳에 쓸곳은 없다. ****
 //      {}브라켓이 있고 return이 있으면 ; 함수로 산출한 값을 반환하여 다른곳으로 넘기거나 코딩줄이 여러개일때 원하는 반환값의 함수에 return을 쓰고 종료시킨다.
 //      * {}브라켓을 쓸때는 return이 있어야 의도한 값을 반환해서 재사용 한다. *
