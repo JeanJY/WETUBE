82149b5d4ca64b9b9dc83d55f0507919b98f8555
Jean <jeanjyjh@gmail.com>
2024-08-26 21:32:04 +1200

    Implement video recording functionality (1)
    - Improve button event listeners to switch between start and stop functions.
    - Add 10-second automatic recording stop.

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,13 +1,42 @@
 const startBtn = document.getElementById("startBtn");
 const previewVideo = document.getElementById("preview");
 
-const handleStartRecording = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
-    audio: true,
+let stream;
+
+const handleStopRecording = () => {
+  startBtn.innerText = "Start Recording";
+  startBtn.removeEventListener("click", handleStopRecording);
+  startBtn.addEventListener("click", handleStartRecording);
+};
+
+const handleStartRecording = () => {
+  startBtn.innerText = "Stop Recording";
+  startBtn.removeEventListener("click", handleStartRecording);
+  startBtn.addEventListener("click", handleStopRecording);
+
+  const recorder = new MediaRecorder(stream);
+  recorder.ondataavailable = (e) => {
+    console.log("recording done");
+    console.log(e);
+    console.log(e.data);
+  };
+  console.log(recorder);
+  recorder.start();
+  console.log(recorder);
+  setTimeout(() => {
+    recorder.stop();
+  }, 10000);
+};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
+    audio: false,
     video: true,
   });
   previewVideo.srcObject = stream;
   previewVideo.play();
 };
+
+init();
 
 startBtn.addEventListener("click", handleStartRecording);
