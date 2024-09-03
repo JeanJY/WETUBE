8c2fa16b1ebbb7a37aca52ebc3fc603b0b9dd0e0
Jean <jeanjyjh@gmail.com>
2024-09-04 11:48:07 +1200

    Implement backend for comment functionality
    - Set up API routes for comment creation
    - Add controller function for handling comment submissions
    - Implement frontend(assets-commentSection.js) for sending comments to the server
    - Add JSON parsing middleware to handle comment data ("JavaScript object → JSON string → JavaScript object")
    - Restrict comment functionality to logged-in users only

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,12 +1,23 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
-const textarea = form.querySelector("textarea");
-const btn = form.querySelector("button");
 
 const handleSubmit = (event) => {
   event.preventDefault();
-  const text = textarea.value;
-  const video = videoContainer.dataset.id;
+  const videoId = videoContainer.dataset.id;
+  const textarea = form.querySelector("textarea");
+  const text = textarea.value.trim();
+  if (text === "") {
+    return;
+  }
+  fetch(`/api/videos/${videoId}/comment`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ text }), // 자바스크립트 오브젝트를 받아서 string으로 바꿔서 인터넷으로 보내고 Server.js로 가면 ->
+  });
 };
 
-form.addEventListener("submit", handleSubmit);
+if (form) {
+  form.addEventListener("submit", handleSubmit);
+}
--- src/db.js
+++ src/db.js
@@ -8,5 +8,4 @@ const handleOpen = () => console.log("Connected to DB!");
 db.once("open", handleOpen);
 db.on("error", (handleError) => console.log("DB Error, error"));
 
-//????
-// on과 once의 차이 그리고 니콜라스가 저렇게 사용한 목적은?????
+// on과 once의 차이 그리고 저렇게 사용한 목적은?
